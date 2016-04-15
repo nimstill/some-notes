89789f98a1533ff5f85acd7eaf8433c548a6199c
nimstill <1679917335@qq.com>
2016-04-15 20:33:30 +0800

    add

---
+++ reactnative test.js
@@ -0,0 +1,14 @@
+render: function() {
+    return (
+        <View style = {styles.container}>
+        <Text>
+            this si my first custom view!
+        </Text>
+        <Image
+        source="{{url: }}"
+        />
+        </View>
+    )；
+}
+
+ 
