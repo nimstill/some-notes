9b5f09fd7e4ba68e6ee0654e3cfa2531f48aa477
nimstill <1679917335@qq.com>
2016-04-20 14:13:29 +0800

    add

--- RN-练习.js
+++ RN-练习.js
@@ -26,7 +26,45 @@ getJSON('story.json').then(function(story) {
 }).catch(function(err) {
     addTextToPage("argh, broken: " + err.message);
 }).then(function() {
-    document.querySelector('.spinner').style.display
- = 'none';
+    document.querySelector('.spinner').style.display = 'none';
 });
+
+
+//创建序列
+
+var sequence = Promise.resolve();
+
+story.chapterUrls.forEach(function(chapterUrl) {
+    sequence = sequence.then(function() {
+        return getJSON(chapterUrl);
+    }).then(function(chapter) {
+        addHtmlToPage(chapter.html);
+    });
+});
+
+/*
+forEach
+catch
+then
+*/
+
+getJSON('story.json').thn(function(story) {
+    addHtmlToPage(story.heading);
+
+    return story.chapterUrls.map(getJSON)
+        .reduce(function(sequence, chapterPromise) {
+            return sequence.then(function() {
+                return chapterPromise;
+            }).then(function(chapter) {
+                addHtmlToPage(chapter.html);
+            });
+        },promise.resolve());
+}).then(function() {
+    addTextToPage("All done");
+}).catch(function(err) {
+    addTextToPage("Algh, broken: " + err.message);
+}).then(funcyion() {
+    document.querySelector('.spinnner').style.display = 'none';
+});
+
 
