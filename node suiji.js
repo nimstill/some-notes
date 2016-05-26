ddcbcce4de892f8c8d7b993250ba30d874740289
nimstill <1679917335@qq.com>
2016-05-26 11:40:51 +0800

    notes

--- node suiji.js
+++ node suiji.js
@@ -16,7 +16,31 @@ conn.on('data', function (data) {
             }
         }
     }
-    
+    else {
+        for (var i in users) {
+            if (i != nickname) {
+                users[i].write('\033[96m > ' + nickname + ':\033[39m ' +data + '\n');
+            }
+        }
+    }
+    conn.on('close', function () {
+        count--;
+        delete users[nickname];
+    });
+    function broadcast (msg, exceptMyself) {
+        for (var i in users) {
+            if (!exceptMyself || i != nickname) {
+                users[i].write(msg);
+            }
+        }
+    }
+    conn.on('data', function (data) {
+        broadcast('\033[90m > ' + nickname + 'joined the room\033[39m\n');
+        broadcast('\033[93m > ' +nickname + ':\033[39m ' + data + '\n', true);
+        conn.on('close', function() {
+            broadcast('\033[90m > ' + nickname + 'left the room \033[39\n');
+        });
+    })
 })
 
 
