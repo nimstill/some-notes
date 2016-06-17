04876a0db019f515b11309f9436f1f0dfa6b1549
nimstill <1679917335@qq.com>
2016-06-17 22:33:06 +0800

    notes

--- C#复习.cs
+++ C#复习.cs
@@ -19,3 +19,59 @@ namespace ConsoleApplicationExample
 }
 
 
+long k = 5000;
+int i = (int)k;
+
+int i = 123;
+object o = i;
+int k = (int)o;
+
+Console.Write();
+Console.WriteLine();
+Console.ReadKey();
+Console.ReadLine();
+
+int[] myint={1,2,3};
+foreach (int test in myint)
+{
+    test++;
+    Console.WriteLine(test);
+}
+
+public int IndexOfAny(char[] anyOf)
+
+enum days {Mon, Tue, Wed, Thu, Fri, Sat}
+
+public class MyClass<T>
+{
+    public T MyProp {get; set;}
+    public void Print()
+    {
+        Console.WriteLine(MyProp);
+    }
+}
+
+public class Program
+{
+    public static int j = 20; //ziduan
+    public static void Main()
+    {
+        int j = 30;
+        Console.WriteLine(j);
+        Console.WriteLine(Program.j);
+        Console.ReadLine();
+    }
+}
+
+
+ColorDialog colorDialog() = new ColorDialog();
+if (colorDig.showDialog() == DialogResult.OK)
+{
+    textBox1.ForeColor = colorDig.Color;
+}
+
+ShowReadOnly multiselect
+filename  filter
+
+ExecuteNonQuery  ExecuteReader
+ExecuteScaler() 
