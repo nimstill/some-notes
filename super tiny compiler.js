b193c233ed309518461b720818c4dc4b78a810cc
nimstill <1679917335@qq.com>
2016-10-20 00:18:49 +0800

    notes

--- super tiny compiler.js
+++ super tiny compiler.js
@@ -5,4 +5,27 @@
  */
 
 
+function tokenizer(input) {
+    var current = 0;
+    var tokens = [];
 
+    while (current < input.length) {
+        var char = input[current];
+
+        if (char ==='(') {
+            tokens.push({
+                type: 'paren',
+                value: '('
+            });
+
+            current++;
+
+            continue;
+        }
+
+    // Next we're going to check for a closing parenthesis. We do the same exact
+    // thing as before: Check for a closing parenthesis, add a new token,
+    // increment `current`, and `continue`.
+    385
+    }
+}
