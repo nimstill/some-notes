fbde5e25af54c7be6427599a04aa000bd4d35960
nimstill <1679917335@qq.com>
2017-02-25 22:44:17 +0800

    new note my

--- python 笔记.py
+++ python 笔记.py
@@ -77,3 +77,22 @@ def extendList(val, list=None):
 
 
 
+class A(object):
+    def foo(self, x):
+        print("executing foo(%s,%s)" % (self))
+        print('self:', self)
+    @classmethod
+    def class_foo(cls,x):
+        print("executing class_foo(%s,%s)" % (cls, x))
+        print('cls:', cls)
+    @staticmethod
+    def static_foo(x):
+        print("executing static_foo(%s)" % x)
+
+
+a = A()
+print(a.foo(1))
+print(a.class_foo(1))
+print(a.static_foo(1))
+
+
